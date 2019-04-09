38e809660ecbb3db3587324a192492fe6f94b629
Evan Rose <ekrose@laffont.Berkeley.EDU>
2019-04-09 09:46:43 -0700

    removed old job code

--- jobs.py
+++ jobs.py
@@ -71,21 +71,3 @@ def get_jobs(curr_app, potential_addys, dob):
 	return joblist
 
 
-	# First job is a random choice for the last two years
-	fjob = np.random.choice([k for k in potential_firms if k is not curr_app], 1)[0]
-	fpos = np.random.choice(positions, 1)[0]
-	faddy = potential_addys.sample(1).values[0,0]
-	fsup = np.random.choice(supervisors, 1)[0]
-	fstart = "{}/{}".format(today.month,today.year-2)
-	fend = "Present"
-
-	# Second job
-	sjob = np.random.choice([k for k in potential_firms if k is not curr_app], 1)[0]
-	spos = np.random.choice(positions, 1)[0]
-	saddy = potential_addys.sample(1).values[0,0]
-	ssup = np.random.choice([k for k in supervisors if k is not fsup], 1)[0]
-	sstart = "{}/{}".format(today.month,today.year-3)
-	send = "{}/{}".format(today.month,today.year-2)
-
-	return [(fjob, fpos, faddy, fsup, fstart, fend),
-			(sjob, spos, saddy, ssup, sstart, send)]
