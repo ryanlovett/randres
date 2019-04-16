055876c915a9f3d58af89f8a2ec05de1d88511c7
Evan Rose <ekrose@laffont.Berkeley.EDU>
2019-04-15 21:52:49 -0700

    zipped assets

--- generate_db.py
+++ generate_db.py
@@ -2,6 +2,7 @@
 # Should be executed from directory above instance
 import sqlite3
 import csv
+import gzip
 
 conn = sqlite3.connect('instance/randres.sqlite')
 db = conn.cursor()
@@ -9,22 +10,22 @@ db = conn.cursor()
 with open('randres/schema.sql') as f:
     db.executescript(f.read())
 
-addy_list = 'randres/assets/address_sample.csv'
-with open(addy_list) as fin: 
+addy_list = 'randres/assets/address_sample.csv.gz'
+with gzip.open(addy_list, mode="rt") as fin: 
     dr = csv.DictReader(fin) 
     for i in dr:
-        print(i['number'], i['street'], i['unit'], i['city'], i['region'], i['zipcode']) 
+        # print(i['number'], i['street'], i['unit'], i['city'], i['region'], i['zipcode']) 
         db.execute("INSERT INTO addy (numb, street, unit, city, region, zip) "
                         "VALUES (?, ?, ?, ?, ?, ?)",
                         (i['number'], i['street'], i['unit'], i['city'], i['region'], i['zipcode']))
     conn.commit()
 
 # Add the school list
-schl_list = 'randres/assets/schl_sample.csv'
-with open(schl_list) as fin: 
+schl_list = 'randres/assets/schl_sample.csv.gz'
+with gzip.open(schl_list, mode="rt") as fin: 
     dr = csv.DictReader(fin) 
     for i in dr:
-        print(i['SCHNAM09'], i['LSTREE09'], i['LCITY09'], i['LSTATE09'], i['LZIP09']) 
+        # print(i['SCHNAM09'], i['LSTREE09'], i['LCITY09'], i['LSTATE09'], i['LZIP09']) 
         db.execute("INSERT INTO schl (name, street, city, state, zip) "
                         "VALUES (?, ?, ?, ?, ?)",
                         (i['SCHNAM09'], i['LSTREE09'], i['LCITY09'], i['LSTATE09'], i['LZIP09']))
