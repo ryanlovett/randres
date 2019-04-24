8a8c05a1782f703d0e6cac6173bac3fba1fcd91e
Ryan Lovett <rylo@berkeley.edu>
2019-04-24 15:56:39 -0700

    Listen on all interfaces
    Enables docker to proxy the connection.

--- hello_old.py
+++ hello_old.py
@@ -157,4 +157,4 @@ if __name__ == "__main__":
     # Setting debug to True enables debug output. This line should be
     # removed before deploying a production app.
     app.debug = True
-    app.run()
+    app.run(host='0.0.0.0')
